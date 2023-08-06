0a9444639971f748ae5e33c49a6729b87ec4c0ab
Petr Knap <8299754+petrknap@users.noreply.github.com>
2023-08-06 21:01:22 +0000

    chore: reformatted

--- ssdp-faker.js
+++ ssdp-faker.js
@@ -1,63 +1,61 @@
-const script = process.argv[1],
-    processArgs = process.argv.slice(2),
-    ssdp = require('node-ssdp'),
-    action = processArgs[0],
-    location = processArgs[1],
-    usns = processArgs.slice(2);
+const script = process.argv[1]
+const processArgs = process.argv.slice(2)
+const ssdp = require('node-ssdp')
+const action = processArgs[0]
+const location = processArgs[1]
+const usns = processArgs.slice(2)
 
 switch (action) {
-    case 'scan-network':
-        scanNetwork();
-        break;
-    case 'run-server':
-        runServer(location, usns);
-        break;
-    default:
-        console.log('Unsupported action %o', action);
-        console.log('');
-    case '-h':
-    case '--help':
-    case 'help':
-        console.log('Usage: node %s scan-network', script);
-        console.log('       node %s run-server location [USN1 ...]', script);
-        console.log('');
-        console.log('Visit https://github.com/petrknap/ssdp-faker for more information.');
-        break;
+  case 'scan-network':
+    scanNetwork()
+    break
+  case 'run-server':
+    runServer(location, usns)
+    break
+  default: // eslint-disable-line default-case-last
+    console.log('Unsupported action %o', action)
+    console.log('')
+  case '-h': // eslint-disable-line no-fallthrough
+  case '--help':
+  case 'help':
+    console.log('Usage: node %s scan-network', script)
+    console.log('       node %s run-server location [USN1 ...]', script)
+    console.log('')
+    console.log('Visit https://github.com/petrknap/ssdp-faker for more information.')
+    break
 }
 
-function scanNetwork() {
-    var client = new ssdp.Client(),
-        serviceType= 'ssdp:all';
+function scanNetwork () {
+  const client = new ssdp.Client()
+  const serviceType = 'ssdp:all'
 
-    client.on('response', function (headers) {
-        console.log(headers);
-    });
+  client.on('response', function (headers) {
+    console.log(headers)
+  })
 
-    console.log('Scanning network for %o', serviceType)
-    client.search(serviceType);
+  console.log('Scanning network for %o', serviceType)
+  client.search(serviceType)
 
-    setTimeout(function () {
-        console.log('Scan complete')
-    }, 5000);
+  setTimeout(function () {
+    console.log('Scan complete')
+  }, 5000)
 }
 
-function runServer(location, usns) {
-    var server = new ssdp.Server({
-        'location': location
-    });
-    console.log('Location set to %o', location);
+function runServer (location, usns) {
+  const server = new ssdp.Server({ location })
+  console.log('Location set to %o', location)
 
-    server.addUSN('upnp:rootdevice');
-    usns.forEach(function (usn) {
-        server.addUSN(usn);
-        console.log('USN %o added', usn);
-    });
+  server.addUSN('upnp:rootdevice')
+  usns.forEach(function (usn) {
+    server.addUSN(usn)
+    console.log('USN %o added', usn)
+  })
 
-    server.start();
-    console.log('Server started');
+  server.start()
+  console.log('Server started')
 
-    process.on('exit', function () {
-        server.stop();
-        console.log('Server stopped');
-    });
+  process.on('exit', function () {
+    server.stop()
+    console.log('Server stopped')
+  })
 }
